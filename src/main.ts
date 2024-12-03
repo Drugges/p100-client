84f7a2ababdf1ea58051706c2eccaeb0a73cad76
Peter Drugge <drugges@gmail.com>
2024-12-03 12:54:32 +0100

    no message

--- src/main.ts
+++ src/main.ts
@@ -1,115 +1,28 @@
 import './style.css'
 
-import GameScene from "./GameScene";
+import { App } from "p100-lib";
 
-const scene = new GameScene();
+const API_URL = import.meta.env.VITE_API_URL;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 
-const project: any = {
-  _id: "123",
-  name: "Test Project",
-  nodes: [
-    {
-      name: "Door-1",
-      type: "entity",
-      primitive: "plane",
-      _id: "1",
-      position: {
-        x: -2.0,
-        y: 0.0,
-        z: -2.5,
-      },
-      rotation: {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-      },
-      scale: {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
-      },
-      components: [
-        {
-          type: "plane-geometry",
-          width: 1,
-          height: 1,
-        },
-        {
-          type: "material",
-          shader: "flat",
-          color: "#FF0000"
-        },
-        {
-          type: "scale-animation",
-          on: {
-            name: "hover-enter",
-            target: "1"
-          },
-          target: { x: 2, y: 2, z: 2 }
-        },
-        {
-          type: "scale-animation",
-          on: {
-            name: "click",
-            target: "1"
-          },
-          target: { x: 1, y: 1, z: 1 }
-        },
-      ],
-      nodes: [],
-    },
-    {
-      name: "Door-2",
-      type: "entity",
-      primitive: "plane",
-      _id: "2",
-      position: {
-        x: 2.0,
-        y: 0.0,
-        z: -2.5,
-      },
-      rotation: {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-      },
-      scale: {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
-      },
-      components: [
-        {
-          type: "plane-geometry",
-          width: 1,
-          height: 1,
-        },
-        {
-          type: "material",
-          shader: "flat",
-          color: "#FF0000"
-        },
-        {
-          type: "scale-animation",
-          on: {
-            name: "hover-enter",
-            target: "1"
-          },
-          target: { x: 2, y: 2, z: 2 }
-        },
-        {
-          type: "scale-animation",
-          on: {
-            name: "click",
-            target: "1"
-          },
-          target: { x: 1, y: 1, z: 1 }
-        },
-      ],
-      nodes: [],
-    },
-  ],
-};
+let app: App | null = null;
 
-//scene.renderProject(project);
+async function run() {
+  document.removeEventListener("click", run);
 
+  const div = document.querySelector("#click-msg");
+  if (div) {
+    div.parentElement!.removeChild(div);
+  }
+
+  const canvas = document.querySelector<HTMLCanvasElement>('#app')!;
+  app = new App(canvas, API_URL, PROJECT_ID);
+  await app.init();
+}
+
+async function init() {
+  console.log("init()");
+  document.addEventListener("click", run);
+}
+
+init();
